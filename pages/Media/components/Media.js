adf5bf577e381c087ed8ae050bf7e74c3767abab
gualin <iiwwpr1@gmail.com>
2023-06-19 11:13:31 +0800

    feat:新增media頁面

--- pages/Media/components/Media.js
+++ pages/Media/components/Media.js
@@ -1,70 +1,74 @@
-import { useState } from "react";
-import BoxTitle from "@/pages/components/IconTextBox/BoxTitle";
-import EventBox from "@/pages/components/EventBox/EventBox";
-import { Button } from "antd";
-import Link from "next/link";
-
-const YEAR = ["全部", "2023"];
-const EVENTS_LIST = [
-  { title: "Unparalleled joy of thematic gaming", time: "2023.03.09" },
-  { title: "SimplePlay 獲得首個重要獎項！", time: "2023.03.06" },
-  { title: "SimplePlay 榮獲 SPiCE India Awards", time: "2023.03.01" },
-  { title: "您的美麗　是贏獎金的助力", time: "2023.02.23" },
+import TitleComponent from "@/pages/components/TitleComponent/TitleComponent";
+import CommonWrapper from "@/pages/components/Wrapper/CommonWrapper";
+import React, { useRef } from "react";
+import CommonSwiper from "../../components/CommonSwiper/CommonSwiper";
+const selectTag = [
+  {
+    label: "All",
+  },
+  {
+    label: "2023",
+  },
 ];
+const carouselList = [
+  {
+    label: "East & West Virtual Expo:",
+    title: "New Wave",
+    date: "2021.06.01 - 2021.06.01",
+    img: "card1",
+  },
+  {
+    label: "East & West Virtual Expo:",
+    title: "New Wave",
+    date: "2021.06.01 - 2021.06.01",
+    img: "card2",
+  },
+  {
+    label: "East & West Virtual Expo:",
+    title: "New Wave",
+    date: "2021.06.01 - 2021.06.01",
+    img: "card3",
+  },
+  {
+    label: "East & West Virtual Expo:",
+    title: "New Wave",
+    date: "2021.06.01 - 2021.06.01",
+    img: "card4",
+  },
+  {
+    label: "East & West Virtual Expo:",
+    title: "New Wave",
+    date: "2021.06.01 - 2021.06.01",
+    img: "card1",
+  },
+  {
+    label: "East & West Virtual Expo:",
+    title: "New Wave",
+    date: "2021.06.01 - 2021.06.01",
+    img: "card2",
+  },
+];
+
 const Media = () => {
-  const [tab, setTab] = useState(YEAR[0]);
   return (
-    <section className="bg-[#202123]">
-      <section className="lg:pt-[250px] md:pt-[150px] pt-[100px] pb-[0px] container mx-auto">
-        <BoxTitle
-          white
-          title={`新聞發稿`}
-          content={`此欄載有我們最新的產品資訊，並有相關遊戲圖片供免費下載，以作發布之用。`}
+    <CommonWrapper className={`w-full`}>
+      <section className="flex flex-col gap-[30px] justify-center h-full">
+        <TitleComponent
+          title="MEDIA"
+          content="此欄截有我們最新的產品資訊，並有相關遊戲圖片提供免費下載，以作發布之用。"
+          selectTag={selectTag}
         />
-      </section>
-      <section className="lg:px-[200px] md:px-[100px] container mx-auto">
-        <div className="flex py-[15px] px-[10px] text-white border-b-2 border-red-500  mb-[30px]">
-          {YEAR.map((year, index) => {
-            return (
-              <p
-                key={index}
-                className={`leading-[40px] mr-[30px] text-2xl max-[640px]:text-lg cursor-pointer footer-text-bottom ${
-                  tab === year && "footer-text-bottom-show"
-                }`}
-                onClick={() => setTab(year)}
-              >
-                {year}
-              </p>
-            );
-          })}
+        <div className="flex gap-[10px] items-center relative overflow-hidden">
+          <div className="max-w-[1340px] px-[50px] flex">
+            <CommonSwiper folderName={`Media`} carouselList={carouselList} />
+          </div>
+          <div
+            className="bg-white absolute w-full h-full"
+            style={{ top: "27%" }}
+          ></div>
         </div>
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 px-[20px] py-[0px]">
-          {EVENTS_LIST.map((event, index) => {
-            return (
-              <Link key={index} href={`/Media/${index}`}>
-                <EventBox
-                  KEY={index}
-                  imageSrc={`/Images/Media/event${index + 1}.jpeg`}
-                  title={event.title}
-                  time={event.time}
-                  width="w-full"
-                />
-              </Link>
-            );
-          })}
-        </div>
-        <section className="flex items-center justify-center pb-[30px]">
-          <Button
-            className="w-[200px] hover:bg-red-500 hover:!text-white hover:translate-y-[10px] transition duration-300"
-            shape="round"
-            size="large"
-            danger
-          >
-            顯示更多
-          </Button>
-        </section>
       </section>
-    </section>
+    </CommonWrapper>
   );
 };
 
--- pages/Media/index.js
+++ pages/Media/index.js
@@ -1,14 +1,18 @@
-import Footer from "../components/Footer/Footer";
+import FooterNew from "../components/Footer/FooterNew";
 import Header from "../components/Header/Header";
+import NavbarMenuList from "../components/Header/NavbarMenuList";
+import Wrapper from "../components/Wrapper/Wrapper";
 import Media from "./components/Media";
+// import About from "./components/About";
 
 const Index = () => {
   return (
-    <>
+    <Wrapper commonPadding>
       <Header />
+      <NavbarMenuList />
       <Media />
-      <Footer />
-    </>
+      <FooterNew />
+    </Wrapper>
   );
 };
 
