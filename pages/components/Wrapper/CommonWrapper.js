e784f2af6bb778fb6e1a4dfd4130211a87b2cca0
Sylvia <sylvia9517@gmail.com>
2023-06-30 09:36:50 +0800

    chore:內頁max-width

--- pages/components/Wrapper/CommonWrapper.js
+++ pages/components/Wrapper/CommonWrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CommonWrapper = ({ children, className }) => {
   return (
-    <main className={`${className && className} max-w-[1500px] pl-[5%] pt-[12%] max-[1024px]:pt-[25%] max-[1024px]:pl-0`}>{children}</main>
+    <main className={`${className && className} max-w-[1600px] pl-[5%] pt-[12%] max-[1024px]:pt-[25%] max-[1024px]:pl-0`}>{children}</main>
   );
 };
 
