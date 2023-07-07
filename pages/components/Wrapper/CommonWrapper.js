bb6e799b8e781ea61063db57455237e7ed5f61a6
Sylvia <sylvia9517@gmail.com>
2023-07-07 10:11:28 +0800

    chore:內頁padding

--- pages/components/Wrapper/CommonWrapper.js
+++ pages/components/Wrapper/CommonWrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CommonWrapper = ({ children, className }) => {
   return (
-    <main className={`${className && className} max-w-[1600px] pl-[5%] pt-[12%] min-[1800px]:pt-[8%] min-[2700px]:pt-[5%] max-[1024px]:pt-[17%] max-[1024px]:pl-0`}>{children}</main>
+    <main className={`${className && className} max-w-[1600px] pl-[5%] pt-[9%] min-[1800px]:pt-[8%] min-[2700px]:pt-[5%] max-[1024px]:pt-[17%] max-[1024px]:pl-0`}>{children}</main>
   );
 };
 
