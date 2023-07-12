b9cde8a7157b1b4808b61272e7d9bc96bcaddc53
gualin <iiwwpr1@gmail.com>
2023-07-12 10:09:14 +0800

    fix:footer隱藏（會擋到按鈕）

--- pages/components/Footer/FooterNew.js
+++ pages/components/Footer/FooterNew.js
@@ -4,7 +4,7 @@ import ContactUsNew from "./ContactUsNew";
 
 const FooterNew = () => {
   return (
-    <footer className="fixed bottom-0 left-[10%] z-[2] max-[1024px]:left-auto max-[1024px]:ml-auto max-[1024px]:w-full max-[1024px]:flex max-[1024px]:justify-center">
+    <footer className="!hidden fixed bottom-0 left-[10%] z-[2] max-[1024px]:left-auto max-[1024px]:ml-auto max-[1024px]:w-full max-[1024px]:flex max-[1024px]:justify-center">
       {/* <ContactInput /> */}
       <ContactUsNew />
     </footer>
