5d4445967995d9a15b04e775262e1e705a35be8f
sz9517 <sylvia9517@gmail.com>
2023-09-07 14:50:54 +0800

    feat:設定環境變數 .env

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
   reactStrictMode: true,
   output: "out",
   trailingSlash: true,
-  assetPrefix: "https://egslot.net",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? process.env.ASSET_PREFIX : "",
+  // assetPrefix: "https://egslot.net",
+  // assetPrefix: "",
   images: {
     domains: ["egslot.net"],
     path: "https://egslot.net",
